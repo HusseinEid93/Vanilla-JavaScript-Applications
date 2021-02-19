7e51a9f82a73b8a42aacd7495b437bc448b55c16
HusseinEidAbdelgelil <heid@sci.cu.edu.eg>
2021-02-19 01:47:56 -0800

    Adding local storage

--- App 18 - Background changer/script.js
+++ App 18 - Background changer/script.js
@@ -2,6 +2,10 @@ const changeBtn = document.getElementById('change');
 const colorText = document.getElementById('color');
 const body = document.body;
 
+// Get the background color from the local storage or set it to white if there is no local storage. 
+body.style.backgroundColor
+   = localStorage.getItem('bg') || 'white';
+
 
 changeBtn.addEventListener('click', changeBackground);
 
@@ -14,6 +18,9 @@ function changeBackground() {
    const colorString = `rgb(${red}, ${green}, ${blue})`;
 
    body.style.backgroundColor = colorString;
+
+   // Save the color in the local storage. 
+   localStorage.setItem('bg', colorString);
 
    colorText.innerText = colorString;
 }
